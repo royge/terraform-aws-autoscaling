5b8c8b890edb045c7a45025cd1c0ffd7b6623c08
Roy E <royevangelista@gmail.com>
2019-03-04 21:47:02 +0800

    Add description to variables

--- variables.tf
+++ variables.tf
@@ -1,26 +1,38 @@
 # AWS Region
-variable "region" {}
+variable "region" {
+  desription = "AWS region"
+}
 
 variable "min" {
   default = 1
+  description = "Minimum number of EC2 instances"
 }
 
 variable "max" {
   default = 2
+  description = "Maximum number of EC2 instances"
 }
 
 variable "cap" {
   default = 1
+  description = "Desired number of EC2 instances."
 }
 
 variable "security_groups" {
   type = "list"
+  description = "List of security group IDs"
 }
 
-variable "instance_type" {}
-variable "key_name" {}
+variable "instance_type" {
+  description = "EC2 instance type to be used"
+}
+variable "key_name" {
+  description = "Keypair name"
+}
 
-variable "name" {}
+variable "name" {
+  description = "Namespace"
+}
 
 variable "metrics" {
   type = "list"
@@ -31,23 +43,31 @@ variable "metrics" {
     "GroupInServiceInstances",
     "GroupTotalInstances",
   ]
+  description = "Autoscaling metrics"
 }
 
 variable "ami_names" {
   type = "list"
+  description = "AMI names filter"
 }
 
 variable "ami_owners" {
   type = "list"
+  description = "AMI owner"
 }
 
-variable "domain" {}
+variable "domain" {
+  description = "Domain name"
+}
 variable "public_security_groups" {
   type = "list"
+  description = "Publicly accessible security group IDs"
 }
 variable "subnets" {
   type = "list"
+  description = "List of subnet IDs"
 }
 variable "health_check_target" {
   default = "HTTPS:443/"
+  description = "ELB health check URL"
 }
